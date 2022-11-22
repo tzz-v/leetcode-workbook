62ed88dfcaa55470ed1bb6525c193eb58754e644
tianzuzhao <tianzuzhao@actionsky.com>
2022-11-22 10:47:36 +0800

    add question 509

---
+++ 十一、动态规划/1.(509)斐波那契数/index.ts
@@ -0,0 +1,21 @@
+// 直接递归
+// function fib(n: number): number {
+//   if (n === 0) return 0;
+//   if (n === 1) return 1;
+//   return fib(n - 1) + fib(n - 2);
+// };
+
+// 动态规划
+// 建立一个dp数组
+// 确认公式： dp[i] = dp[i - 1] + dp[i - 2];
+// 遍历推导 得到最终的dp[i];
+function fib(n: number): number {
+  if (n <= 1) return n;
+  const dp = [0, 1];
+  for (let i = 2; i <= n; i++) {
+    const val = dp[0] + dp[1];
+    dp[0] = dp[1];
+    dp[1] = val;
+  }
+  return dp[1];
+}
