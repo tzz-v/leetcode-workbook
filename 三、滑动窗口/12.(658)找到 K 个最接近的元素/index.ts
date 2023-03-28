a579c2ae8b7a33df96d2890724fef3cf9ebd7ca9
tianzuzhao <tianzuzhao@actionsky.com>
2023-03-28 17:25:30 +0800

    add question 658

---
+++ 三、滑动窗口/12.(658)找到 K 个最接近的元素/index.ts
@@ -0,0 +1,28 @@
+// 在arr中找到最接近数字x的下标项当窗口的右边界；
+// 定义左边界为right - k；
+// 滑动窗口。
+// 得到最后的左右边界
+
+// 注：可以通过二分 来查找最接近的数字，将低查找部分的时间复杂度。
+
+function findClosestElements(arr: number[], k: number, x: number): number[] {
+  if (x < arr[0]) {
+    return arr.slice(0, k);
+  }
+  const len = arr.length;
+  let right = 0;
+  while (x >= arr[right]) {
+    right++;
+  }
+  right = Math.max(right, k);
+  let left = right - k;
+
+  while (right < len) {
+    if (Math.abs(arr[left] - x) <= Math.abs(arr[right] - x)) {
+      break;
+    }
+    right++;
+    left++;
+  }
+  return arr.slice(left, right);
+}
