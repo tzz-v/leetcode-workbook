4e18d5cb25b9d513035490a966a3f1af605142a6
tianzuzhao <tianzuzhao@actionsky.com>
2023-06-19 13:29:37 +0800

    add question2725

---
+++ 十三、JavaScript专栏/42.(2725)间隔取消/index.ts
@@ -0,0 +1,36 @@
+function cancellable(fn: Function, args: any[], t: number): Function {
+  fn(...args);
+  let timer = setInterval(fn.bind(null, ...args), t);
+  return () => {
+    clearInterval(timer);
+    timer = null;
+  };
+}
+
+/**
+ *  const result = []
+ *
+ *  const fn = (x) => x * 2
+ *  const args = [4], t = 20, cancelT = 110
+ *
+ *  const start = performance.now()
+ *
+ *  const log = (...argsArr) => {
+ *		const val = fn(...argsArr)
+ *      result.push({"time": Math.floor(performance.now() - start), "returned": fn(...argsArr)})
+ *  }
+ *
+ *  const cancel = cancellable(log, args, t);
+ *
+ *  setTimeout(() => {
+ *     cancel()
+ *     console.log(result) // [
+ *                         //      {"time":0,"returned":8},
+ *                         //      {"time":20,"returned":8},
+ *                         //      {"time":40,"returned":8},
+ *                         //      {"time":60,"returned":8},
+ *                         //      {"time":80,"returned":8},
+ *                         //      {"time":100,"returned":8}
+ *                         //  ]
+ *  }, cancelT)
+ */
