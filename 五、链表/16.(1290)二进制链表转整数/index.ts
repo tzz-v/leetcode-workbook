9bbb6a26f062ffdadcae3874146734d8806231ea
tianzuzhao <tianzuzhao@actionsky.com>
2023-08-20 21:50:12 +0800

    add question 1290

---
+++ 五、链表/16.(1290)二进制链表转整数/index.ts
@@ -0,0 +1,20 @@
+/**
+ * Definition for singly-linked list.
+ * class ListNode {
+ *     val: number
+ *     next: ListNode | null
+ *     constructor(val?: number, next?: ListNode | null) {
+ *         this.val = (val===undefined ? 0 : val)
+ *         this.next = (next===undefined ? null : next)
+ *     }
+ * }
+ */
+
+function getDecimalValue(head: ListNode | null): number {
+  let res = 0;
+  while (head) {
+    res = res * 2 + head.val;
+    head = head.next;
+  }
+  return res;
+}
