7d4e1d2cbf8b381442f763a20c25489bfcc330f3
tianzuzhao <tianzuzhao@actionsky.com>
2023-11-24 11:04:24 +0800

    add question2269

--- nodedemo.js
+++ nodedemo.js
@@ -1,25 +1,31 @@
+const fs = require('fs');
 const func = () => {
-  setTimeout(() => {
-    console.log('timeout0');
-    setTimeout(() => {
-      console.log('timeout2');
-    }, 20);
-    new Promise((resolve, reject) => {
-      setTimeout(() => {
-        resolve('timeout resolved');
-      });
-    }).then((res) => console.log(res));
-    new Promise((resolve, reject) => {
-      resolve('resolved');
-    })
-      .then((res) => {
-        wait(3000);
-        console.log(res);
-      })
-      .then(() => {
-        console.log('你是最后吗');
-      });
-  }, 0);
+  setTimeout(() => console.log('setTimeout'), 1000);
+
+  new Promise((res) => {
+    console.log('promise before 1');
+    res();
+  }).then(() => {
+    console.log('promise 1');
+    new Promise((res) => {
+      console.log('promise before 2');
+      res();
+    }).then(() => {
+      console.log('promise 2');
+    });
+  });
+
+  fs.readFile('./poll.js', () => {
+    console.log('read 1');
+    fs.readFile('./poll.js', () => {
+      console.log('read 2');
+    });
+    wait(2000);
+  });
+  fs.readFile('./poll.js', () => {
+    console.log('read 3');
+    wait(20);
+  });
 };
 
 // 定义一个 wait 方法
