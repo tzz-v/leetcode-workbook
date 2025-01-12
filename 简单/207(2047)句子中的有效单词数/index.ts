efc2cdc06ef6b2c37a9b63440376373eb595d620
tianzuzhao <tianzuzhao@actionsky.com>
2025-01-12 19:46:55 +0800

    add question2047

---
+++ 简单/207(2047)句子中的有效单词数/index.ts
@@ -0,0 +1,10 @@
+function countValidWords(sentence: string): number {
+  const arr = sentence.split(/\s+/);
+  console.log(arr);
+  return arr.reduce((prev, str) => {
+    if (/\d/.test(str) || str.match(/[-!.,]/g).length > 1) return prev;
+    if (/[!.,]/.test(str) && !/[!.,]/.test(str[str.length - 1])) return prev;
+    if (str[0] === '-' || str[str.length - 1] === '-') return prev;
+    return prev + 1;
+  }, 0);
+}
